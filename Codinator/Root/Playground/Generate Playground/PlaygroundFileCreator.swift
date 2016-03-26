a5ab4aa77bf2cb5b0ab1e9208f236c260a090fd6
Vladimir Danila <vwas@vwas.pfweb.eu>
2016-03-26 20:30:17 +0100

    Refactoring

---
+++ Codinator/Root/Playground/Generate Playground/PlaygroundFileCreator.swift
@@ -0,0 +1,13 @@
+//
+//  PlaygroundFileCreator.swift
+//  Codinator
+//
+//  Created by Vladimir Danila on 26/03/16.
+//  Copyright © 2016 Vladimir Danila. All rights reserved.
+//
+
+import UIKit
+
+class PlaygroundFileCreator: NSObject {
+
+}
